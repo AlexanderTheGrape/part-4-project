05949b0d144470595aa95339a296a02209a5fbec
Alex Monk <amonk446@gmail.com>
2020-09-29 10:04:57 +1300

    alternative buffer

--- python-server/server.py
+++ python-server/server.py
@@ -6,22 +6,43 @@ from marytts_helper import generateWav
 # MaryTTS startup stuff here
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 8089))
+serversocket.bind(('localhost', 8080))
 serversocket.listen(5) # become a server socket, maximum 5 connections
 
-while True:
+def main():
+    while True:
+
+        # last_buffer_size = 0
+        # if len(buf) > 0:
+            
+        #     while len(buf) > last_buffer_size:
+        #         last_buffer_size = len(buf)
+            buffer = receiveText()
+
+            textToSynthesize = buffer.decode("utf-8")
+            print("received message: " + textToSynthesize)
+            
+            # now that we have the text for marytts, run it through marytts.
+            # generateWav(textToSynthesize)
+            
+def receiveText():
     connection, address = serversocket.accept()
     # may need to move while loop here
-    buf = connection.recv(1024)
+    buffer = connection.recv(1024)
+    decoded_buffer = buffer.decode()
 
-    last_buffer_size = 0
-    if len(buf) > 0:
-        
-        while len(buf) > last_buffer_size:
-            last_buffer_size = len(buf)  
-        textToSynthesize = buf.decode("utf-8") 
-        print("received message: " + textToSynthesize)
-        
-        # now that we have the text for marytts, run it through marytts.
-        generateWav(textToSynthesize)
-        
+    buffering = True
+    while buffering:
+        if "\n" in buffer:
+            (line, buffer) = buffer.split("\n", 1)
+            return line + "\n"
+        else:
+            more = connection.recv(1024)
+            if not more:
+                buffering = False
+            else:
+                buffer += more
+    if buffer:
+        return buffer
+
+main()
